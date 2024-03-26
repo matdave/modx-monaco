908eb68e9e91bc885054c72a8bf99e462201b882
matdave <mat@matdave.com>
2024-03-26 09:52:30 -0500

    Initial build of Monaco for MODX

--- deploy.me.sh
+++
@@ -1,9 +0,0 @@
-rsync -avz --delete \
---exclude ".git" \
---exclude "vendor" \
---exclude ".idea" \
---exclude ".DS_Store" \
---exclude "deploy.me.sh" \
---exclude "config.core.php" \
---exclude "node_modules" \
-./ me:/www/pkgs/monaco/
