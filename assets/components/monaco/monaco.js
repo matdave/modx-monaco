fff0f2469a6a5eec4f790f720985e90cd2867201
matdave <mat@matdave.com>
2024-04-08 12:11:53 -0500

    add auto close tag function

--- assets/components/monaco/monaco.js
+++ assets/components/monaco/monaco.js
@@ -6,6 +6,7 @@ Monaco.Editor = function(config, editorConfig) {
 Ext.extend(Monaco.Editor,
     Ext.Component,
     {
+        editor: null,
         renderedEditor: false,
         cfg: {
             selector: '',
@@ -114,6 +115,7 @@ Ext.extend(Monaco.Editor,
                 editor.onDidChangeModelContent((e) => {
                     renderTo.setValue(editor.getValue());
                 });
+                editor.onKeyDown(this.autoCloseTags.bind(this));
                 MODx.load({
                     xtype: 'modx-treedrop',
                     target: editor,
@@ -172,7 +174,101 @@ Ext.extend(Monaco.Editor,
             }
             this.editor.layout();
         },
-        rendered: false,
+        autoCloseTags: function (event) {
+            const editor = this.editor;
+            // select enabled languages
+            const enabledLanguages = ["html", "javascript", "markdown", "modx", "twig", "typescript"];
+
+            const model = editor.getModel();
+            if (!enabledLanguages.includes(model.getLanguageId())) {
+                return;
+            }
+
+            const isSelfClosing = (tag) =>
+                [
+                    "area",
+                    "base",
+                    "br",
+                    "col",
+                    "command",
+                    "embed",
+                    "hr",
+                    "img",
+                    "input",
+                    "keygen",
+                    "link",
+                    "meta",
+                    "param",
+                    "source",
+                    "track",
+                    "wbr",
+                    "circle",
+                    "ellipse",
+                    "line",
+                    "path",
+                    "polygon",
+                    "polyline",
+                    "rect",
+                    "stop",
+                    "use",
+                ].includes(tag);
+
+            // when the user enters '>'
+            if (event.browserEvent.key === ">") {
+                const currentSelections = editor.getSelections();
+
+                const edits = [];
+                const newSelections = [];
+                // potentially insert the ending tag at each of the selections
+                for (const selection of currentSelections) {
+                    // shift the selection over by one to account for the new character
+                    newSelections.push(
+                        new monaco.Selection(
+                            selection.selectionStartLineNumber,
+                            selection.selectionStartColumn + 1,
+                            selection.endLineNumber,
+                            selection.endColumn + 1,
+                        ),
+                    );
+                    // grab the content before the cursor
+                    const contentBeforeChange = model.getValueInRange({
+                        startLineNumber: 1,
+                        startColumn: 1,
+                        endLineNumber: selection.endLineNumber,
+                        endColumn: selection.endColumn,
+                    });
+
+                    // if ends with a HTML tag we are currently closing
+                    const match = contentBeforeChange.match(/<([\w-]+)(?![^>]*\/>)[^>]*$/);
+                    if (!match) {
+                        continue;
+                    }
+
+                    const [fullMatch, tag] = match;
+
+                    // skip self-closing tags like <br> or <img>
+                    if (isSelfClosing(tag) || fullMatch.trim().endsWith("/")) {
+                        continue;
+                    }
+
+                    // add in the closing tag
+                    edits.push({
+                        range: {
+                            startLineNumber: selection.endLineNumber,
+                            startColumn: selection.endColumn + 1, // add 1 to offset for the inserting '>' character
+                            endLineNumber: selection.endLineNumber,
+                            endColumn: selection.endColumn + 1,
+                        },
+                        text: `</${tag}>`,
+                    });
+                }
+
+                // wait for next tick to avoid it being an invalid operation
+                setTimeout(() => {
+                    editor.executeEdits(model.getValue(), edits, newSelections);
+                }, 0);
+            }
+        }
     });
 Monaco.load = function(selector, language = 'html') {
     new Monaco.Editor({},{
